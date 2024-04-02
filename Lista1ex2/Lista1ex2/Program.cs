879489629d9f7cefab4e25f61698c1449e890dea
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-01 21:14:41 -0300

    Add files via upload

---
+++ Lista1ex2/Lista1ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int area;
+
+            Console.Write("Digite o tamanho da aresta de um quadrado: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            area = a * a;
+
+            Console.Write("A área do quadrado é: ");
+            Console.Write(area);
+            Console.Write("m³");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
