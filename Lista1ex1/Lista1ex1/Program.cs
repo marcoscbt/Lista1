1a6662224bd881eefa0ca4aa35a70fa389e07a67
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-01 21:05:25 -0300

    Add files via upload

---
+++ Lista1ex1/Lista1ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+           
+            
+            Console.Write("Digite o valor da base: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite o valor da altura: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            area = b * a;
+
+            Console.Write("A área é igual a: ");
+            Console.Write(area) ;
+            Console.Write("m³");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
