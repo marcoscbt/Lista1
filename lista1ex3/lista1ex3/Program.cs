55463f3101048d2e013f3e8a614300836d6b821d
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-08 17:48:06 -0300

    Add files via upload

---
+++ lista1ex3/lista1ex3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.Write("Digite o tamanho da diagonal do quadrado: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            b = a * a;
+            c = b / 2;
+
+            Console.Write("A área desse quadrado é: ");
+            Console.Write(c);
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
