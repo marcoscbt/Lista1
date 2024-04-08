ffa552a2c91f847f7fdc6faf3f8af42bf4a18056
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-08 17:58:24 -0300

    Add files via upload

---
+++ lista1ex4/lista1ex4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bas;
+            int alt;
+            int area;
+            int resultado;
+
+            Console.Write("Digite o tamanho da base do triangulo: ");
+
+            bas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite a altura do triangulo: ");
+
+            alt = int.Parse(Console.ReadLine());
+
+            area = bas * alt;
+            resultado = area / 2;
+
+            Console.WriteLine();
+
+            Console.Write("A area do triangulo é igual a: ");
+            Console.WriteLine(resultado);
+
+            Console.WriteLine();
+
+        }
+    }
+}
