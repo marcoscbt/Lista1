59d11e4f22ce3d6762a48e4d47dd2f6532ad40d9
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-08 18:28:20 -0300

    Add files via upload

---
+++ lista1ex6/lista1ex6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1;
+            double valor2;
+            int raiz1;
+            int raiz2;
+            int media;
+
+            Console.Write("Digite o primeiro número: ");
+            valor1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            valor2 = double.Parse(Console.ReadLine());
+
+            raiz1 = Math.Sqrt;
+            raiz2 = valor2  valor2;
+
+            //não consegui fazer.
+        }
+    }
+}
