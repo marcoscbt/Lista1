c1ae366f9df210cab593d4b5a43d34c1b99af571
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-15 20:38:13 -0300

    Add files via upload

---
+++ lista1ex8/lista1ex8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double celsius;
+            double fahr;
+
+            Console.WriteLine("Calculadora de Celsius em Fahrenheit:");
+
+            Console.Write("Escreva a temperatura em graus Celsius: ");
+
+            celsius = Double.Parse(Console.ReadLine());
+
+            fahr = celsius * 1.8 + 32;
+
+            Console.WriteLine("A temperatura em Fahrenheit é igual a: {0}", fahr);
+
+            Console.WriteLine();
+        }
+    }
+}
