0eeabaa9cd5f4a4d95c4daa7fb6fb7e6e4a9cb71
marcoscbt <165732101+marcoscbt@users.noreply.github.com>
2024-04-15 20:29:02 -0300

    Add files via upload

---
+++ lista1ex7/lista1ex7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transforme km's em milhas marítimas:");
+
+            double km;
+            double resultado;
+
+            Console.Write("Escreva quantos quilomêtros são da sua casa até a faculdade: ");
+            km = double.Parse(Console.ReadLine());
+
+            resultado = km * 1.852;
+            
+            Console.WriteLine();
+
+            Console.WriteLine("A distância em milhas maritimas é igual a: {0}", resultado);
+
+            Console.WriteLine();
+
+
+            
+        }
+    }
+}
